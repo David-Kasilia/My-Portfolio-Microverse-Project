623c7d9cbd2c787c881569e452a13314daa6126c
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-07-18 21:55:59 +0300

    Fix eslint linter error

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let menu = document.querySelector('.menu-list');
-let menuItems = document.querySelectorAll('.menu-item');
-let hamburger = document.querySelector('.hamburger');
-let menuIcon = document.querySelector('.menu');
-let closeIcon = document.querySelector('.close');
+const menu = document.querySelector('.menu-list');
+const menuItems = document.querySelectorAll('.menu-item');
+const hamburger = document.querySelector('.hamburger');
+const menuIcon = document.querySelector('.menu');
+const closeIcon = document.querySelector('.close');
 
 function mobileMenu() {
-    if (menu.classList.contains("show-menu")){
-        menu.classList.remove("show-menu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("show-menu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
+if (menu.classList.contains("show-menu")){
+    menu.classList.remove("show-menu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+} else {
+    menu.classList.add("show-menu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+}
 }
 
 hamburger.addEventListener("click", mobileMenu);
@@ -22,4 +22,4 @@ menuItems.forEach(
     function(menuItem){
         menuItem.addEventListener("click", mobileMenu);
     }
-)
+);
