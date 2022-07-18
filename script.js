0a81a79a6c0cc8c697ed6fdc20d729b9b200f067
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-07-18 21:27:00 +0300

    Fixing errors

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let menuIcon = document.querySelector('.menu');
 let closeIcon = document.querySelector('.close');
 
 function mobileMenu() {
-    if (menu.classList.contains("showMenu")){
-        menu.classList.remove("showMenu");
+    if (menu.classList.contains("show-menu")){
+        menu.classList.remove("show-menu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
     } else {
-        menu.classList.add("showMenu");
+        menu.classList.add("show-menu");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
     }
