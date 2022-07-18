231b7b2328cdb8937f35797f7ed0178c4496ff7f
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-07-18 16:41:47 +0300

    Menu list display

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 let menu = document.querySelector('.menu-list');
+let menuItems = document.querySelectorAll('.menu-item');
 let hamburger = document.querySelector('.hamburger');
 let menuIcon = document.querySelector('.menu');
 let closeIcon = document.querySelector('.close');
+
+function mobileMenu() {
+    if (menu.classList.contains("showMenu")){
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", mobileMenu);
+
+menuItems.forEach(
+    function(menuItem){
+        menuItem.addEventListener("click", mobileMenu);
+    }
+)
