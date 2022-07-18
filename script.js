c39ecb8d94bfdb34de580a06d7a64754c65b42d9
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-07-18 22:00:19 +0300

    Fix eslint errors

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ const menuIcon = document.querySelector('.menu');
 const closeIcon = document.querySelector('.close');
 
 function mobileMenu() {
-if (menu.classList.contains("show-menu")){
-    menu.classList.remove("show-menu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-} else {
-    menu.classList.add("show-menu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-}
+  if (menu.classList.contains('show-menu')){
+    menu.classList.remove('show-menu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('show-menu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+  }
 }
 
 hamburger.addEventListener("click", mobileMenu);
 
 menuItems.forEach(
-    function(menuItem){
-        menuItem.addEventListener("click", mobileMenu);
-    }
+  function dropMenu (menuItem) {
+    menuItem.addEventListener("click", mobileMenu);
+  }
 );
